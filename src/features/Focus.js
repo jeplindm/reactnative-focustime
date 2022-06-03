e1da55e86bcb86268bdd5ba9dd80bd2c9fc54177
Jeplind Morico Gugundo <jeplind.gugundo@photon.com>
2022-06-03 14:02:13 +0700

    focus-time initial commit

---
+++ src/features/Focus.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { colors } from '../utils/colors';
+import { TextInput } from 'react-native-paper';
+import { RoundedButton } from '../components/RoundedButton';
+import { spacing, fontSizes } from '../utils/sizes';
+
+export const Focus = ({ addSubject }) => {
+  const [subject, setSubject] = useState(null);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Focus On!</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={setSubject}
+          label="What would you like to focus on?"
+        />
+        <View style={styles.buttonContainer}>
+          <RoundedButton
+            title="+"
+            size={50}
+            onPress={() => addSubject(subject)}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+  text: {
+    color: colors.white,
+    paddingTop: spacing.lg,
+    paddingLeft: spacing.lg,
+    fontSize: fontSizes.lg,
+    fontWeight: '700',
+  },
+  inputContainer: {
+    padding: spacing.lg,
+    justifyContent: 'top',
+    flexDirection: 'row',
+  },
+  textInput: {
+    flex: 1,
+    marginRight: spacing.lg,
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+  },
+});
---
+++ src/features/Timer.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Vibration } from 'react-native';
+import { ProgressBar, Colors } from 'react-native-paper';
+import { useKeepAwake } from 'expo-keep-awake';
+import { Countdown } from '../components/Countdown';
+import { RoundedButton } from '../components/RoundedButton';
+import { Timing } from '../components/Timing';
+
+import { spacing } from '../utils/sizes';
+import { colors } from '../utils/colors';
+
+const ONE_SECOND_IN_MS = 1000;
+
+const PATTERN = [
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+];
+
+export const Timer = ({ focusSubject, clearSubject, onTimerEnd }) => {
+  useKeepAwake();
+  const [isStarted, setIsStarted] = useState(false);
+  const [progress, setProgress] = useState(1);
+  const [minutes, setMinutes] = useState(0.1);
+
+  const onEnd = (reset) => {
+    Vibration.vibrate(PATTERN);
+    setIsStarted(false);
+    setProgress(1);
+    reset();
+    onTimerEnd(focusSubject);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.countdown}>
+        <Countdown
+          minutes={minutes}
+          isPaused={!isStarted}
+          onProgress={setProgress}
+          onEnd={onEnd}
+        />
+
+        <View style={{ paddingTop: spacing.xxl }}>
+          <Text style={styles.title}>Focus on:</Text>
+          <Text style={styles.task}>{focusSubject}</Text>
+        </View>
+      </View>
+
+      <View>
+        <ProgressBar
+          style={{ height: spacing.sm }}
+          color={Colors.green300}
+          progress={progress}
+        />
+      </View>
+
+      <View style={styles.timingWrapper}>
+        <Timing onChangeTime={setMinutes} />
+      </View>
+
+      <View style={styles.buttonWrapper}>
+        {!isStarted ? (
+          <RoundedButton title="Start" onPress={() => setIsStarted(true)} />
+        ) : (
+          <RoundedButton title="Pause" onPress={() => setIsStarted(false)} />
+        )}
+      </View>
+
+      <View style={styles.clearSubjectWrapper}>
+        <RoundedButton size={50} title="Reset" onPress={clearSubject} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  countdown: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    flex: 0.3,
+    flexDirection: 'row',
+    padding: spacing.md,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.white,
+  },
+  task: {
+    textAlign: 'center',
+    color: colors.white,
+  },
+  timingWrapper: {
+    flex: 0.1,
+    paddingTop: spacing.xxl,
+    flexDirection: 'row',
+  },
+  clearSubjectWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+});
